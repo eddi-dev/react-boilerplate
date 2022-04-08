d202308269f0055c24c8fa61f9c3f8f20d2763c0
hyeyoom <neoul_chw@icloud.com>
2022-04-09 00:18:06 +0900

    added env setting #2

--- src/app/api/BaseUrls.ts
+++ src/app/api/BaseUrls.ts
@@ -1,1 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_MOIM_APP_API_ROOT
+
+
--- src/pages/DummyPage.tsx
+++ src/pages/DummyPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {API_BASE_URL} from "../app/api/BaseUrls";
 
 const DummyPage = () => {
     return (
         <div>
-            <h1>라우팅 테스트</h1>
-            <p>해시뱅극혐</p>
+            <h1>소셜 로그인</h1>
+            <a href={`${API_BASE_URL}/authentication/oauth2/google`}>Google 로그인</a>
         </div>
     )
 }
